9a3ee7f3db5b2691109049a137a97e6f9b096a1a
mdquayle <misternopants@gmail.com>
2021-08-06 18:30:31 -0400

    commit tree 60601b9729f366701024888ad3fe0182202bc3f5

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,19 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  
+  console.log(projects)
+  let projectCounter = 1
+  const newProject = projects.map((project) => {
+    return <div key={projectCounter++}><h2>{project.name}</h2>
+            <p>{project.about}</p>
+            <p>{project.technologies}</p></div>});
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {newProject}
+      </div>
     </div>
   );
 }
